f9e849d6365a11356d11813974c598c98e2fa875
Ang3el <94862096+Vancltkin@users.noreply.github.com>
2025-05-13 09:23:28 +0300

    Update install-singbox-ui.sh

--- other/install-singbox-ui.sh
+++ other/install-singbox-ui.sh
@@ -1,1 +1,7 @@
+#!/bin/sh
 
+wget -O /root/luci-app-singb.ipk https://github.com/Vancltkin/singb/releases/latest/download/luci-app-singb.ipk
+chmod 0755 /root/luci-app-singb.ipk
+opkg update
+opkg install /root/luci-app-singb.ipk
+/etc/init.d/uhttpd restart
