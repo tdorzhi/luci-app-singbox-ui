d974b437afa78ac4380e3825bd4f7c20e692afc2
Ang3el <94862096+Vancltkin@users.noreply.github.com>
2025-05-13 09:36:05 +0300

    Update install-singbox-ui.sh

--- other/install-singbox-ui.sh
+++ other/install-singbox-ui.sh
@@ -2,6 +2,6 @@
 
 wget -O /root/luci-app-singb.ipk https://github.com/Vancltkin/singb/releases/latest/download/luci-app-singb.ipk
 chmod 0755 /root/luci-app-singb.ipk
-opkg update
+opkg update && opkg install openssh-sftp-server nano curl jq
 opkg install /root/luci-app-singb.ipk
 /etc/init.d/uhttpd restart
